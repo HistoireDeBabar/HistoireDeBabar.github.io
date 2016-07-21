a99921ab50535e69937bdaafe31118d7ab2bbfe7
Paul Barber <p.barber1990@gmail.com>
2016-07-21 21:55:38 +0100

    update text

--- src/app.js
+++ src/app.js
@@ -24,30 +24,30 @@ var model = {
     {
       place: 'Newcastle University',
       position: 'MSc Computer Science',
-      description: 'Where the journey began.  Learning Java, SQL and the basic principles of OOP, and SOLID. For specialist study I focused upon NoSQL Datastores, and how efficient they can store and query Geospatial data.',
+      description: 'Where the journey began. Learning Java, SQL and the basic principles of OOP, and SOLID. For specialist study I focused upon NoSQL Datastores, and how efficient they can store and query Geospatial data.',
     },
     {
       place: 'SaleCycle',
       position: 'Software Engineer',
-      description: 'Where the journey really began.  I Work on a distributed web application to process impression data from across the internet.  Learning not only how to work with technologies such as and .NET and Couchbase.  But also how to architect distributed systems which can scale limitlessly.  Currently I work on a cloud based solution utilizing AWS Lambda and Kinesis.',
+      description: 'Where the journey really began. I Worked on multiple distributed web applications to process millions of requests per day from hundreds of e-commerce website from across the world. These stacks include, but not are not limited too, a distributed .NET event driven system, with Couchbase as the data store to a cloud based serverless architecture running on AWS Lambda.  I programmed in languages from C#, Javascript (Node) and Golang.',
     },
     {
       place: 'Place A Vote',
       position: 'Frontend Developer',
-      description: 'Place A Vote are trying to change democarcy.  Voters can decide how they congressmen and woman vote on every bill that enters congress.  I work on the Frontend applications for web using Angular and Node JS.  As well as developing the iOS and Android versions of the application.',
+      description: 'Place A Vote are trying to change democarcy. It is a social web application to promote discusion and envolvement in US politics. I primarily work on the frontend web application. However, also made small data APIs too. Tools, frameworks and languages primarily used were: Node.js, Express, Gulp, Web-Pack, Angular, React, Redux, Yeoman, Redis, CircleCI (continious delivery), SASS and Convox/Docker.',
     },
     {
       place: 'Associate Level',
       position: 'AWS Developer Cert.',
-      description: 'The AWS Certified Developer – Associate exam validates technical expertise in developing and maintaining applications on the AWS platform. Exam concepts you should understand for this exam include: Picking the right AWS services for the application, Leveraging AWS SDKs to interact with AWS services from your application, Writing code that optimizes performance of AWS services used by your application, Code-level application security (IAM roles, credentials, encryption, etc.)',
+      description: 'The AWS Certified Developer – Associate exam validates technical expertise in developing and maintaining applications on the AWS platform. Exam concepts you must understand for this exam include: Picking the right AWS services for the application, Leveraging AWS SDKs to interact with AWS services from your application, Writing code that optimizes performance of AWS services used by your application, Code-level application security (IAM roles, credentials, encryption, etc.)',
     },
   ],
 };
 
 var controller = {
-  init : function(){
+  init : function() {
   },
-  getContacts : function(){
+  getContacts : function() {
     return model.contacts;
   },
   getWelcome: function() {
@@ -61,7 +61,7 @@ var controller = {
 
 var welcome_view = {
   sections: undefined,
-  init: function(){
+  init: function() {
     this.title = document.createElement("div");
     document.body.appendChild(this.title);
     this.render();
@@ -189,10 +189,8 @@ var journey_view = {
 
 
 
-(function(){
-
+(function() {
   menu_view.init();
   welcome_view.init();
   journey_view.init();
-
 })();
